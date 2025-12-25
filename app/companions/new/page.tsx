e6ead9f3928be3768c3213ea1c66679c2001a83d
unknown <khobragadenischay23@gmail.com>
2025-12-25 23:45:09 +0530

    Implement Home page UI

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,7 +1,12 @@
-import React from "react";
-
-const newCompanion = () => {
-  return <div>newCompanion</div>;
+import CompanionForm from "@/components/CompanionForm";
+const NewCompanion = () => {
+  return (
+    <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
+      <article className="w-full gap-4 flex flex-col">
+        <h1>Companion Builder</h1>
+        <CompanionForm />
+      </article>
+    </main>
+  );
 };
-
-export default newCompanion;
+export default NewCompanion;
